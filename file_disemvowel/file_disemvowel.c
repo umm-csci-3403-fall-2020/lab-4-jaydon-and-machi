9a6cbab8686f89caaa8ae59cd0d3d5be3c8dabc2
smit9025 <smit9025@morris.umn.edu>
2020-09-29 10:08:40 -0500

    Initial creation of file_disemvowel.c Created file file_disemvowel.c and gave it some structure.

---
+++ file_disemvowel/file_disemvowel.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define BUF_SIZE 1024
+
+bool is_vowel(char c) {
+
+}
+
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+
+}
+
+int main(int argc, char *argv[]) {
+
+	FILE *inputFile;
+	FILE *outputFile;
+
+	disemvowel(inputFile, outputFile);
+
+	return 0;
+}
