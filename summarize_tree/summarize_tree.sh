e8d96f732cd9204fe4915fa1d1b3703efe657698
Jaydon Smith <46760134+smit9025@users.noreply.github.com>
2020-10-06 11:11:28 -0500

    Update summarize_tree.sh
    Add comments

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -3,8 +3,8 @@
 here=$(pwd)
 cd "$1"
 
-num_file=$(find  -type f | wc -l)
-num_dir=$(find  -type d | wc -l)
+num_file=$(find  -type f | wc -l) #Find file types and count
+num_dir=$(find  -type d | wc -l) #Find directory types and count
 
 echo "There were" "$num_dir" "directories."
 echo "There were" "$num_file" "regular files."
