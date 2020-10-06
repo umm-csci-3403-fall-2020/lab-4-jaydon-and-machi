309c4b16345b6991b86fc5ceedde6392d6b29964
smit9025 <smit9025@morris.umn.edu>
2020-10-06 11:06:16 -0500

    Fix summarize_tree.sh output We fixed the output so that it outputs properly for the test to pass.

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -6,8 +6,8 @@ cd "$1"
 num_file=$(find  -type f | wc -l)
 num_dir=$(find  -type d | wc -l)
 
-echo "There are" "$num_file" "files"
-echo "There are" "$num_dir" "directories"
+echo "There were" "$num_dir" "directories."
+echo "There were" "$num_file" "regular files."
 
 cd "$here"
 
