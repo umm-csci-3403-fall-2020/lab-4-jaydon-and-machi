fd8c9a0e65c65606b6a0a5f112b8aa9481852648
iwata008 <iwata008@morris.umn.edu>
2020-10-06 10:57:02 -0500

    Edit summarize_tree.sh

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+here=$(pwd)
+cd "$1"
 
+num_file=$(find  -type f | wc -l)
+num_dir=$(find  -type d | wc -l)
+
+echo "There are" "$num_file" "files"
+echo "There are" "$num_dir" "directories"
+
+cd "$here"
 
 
 
